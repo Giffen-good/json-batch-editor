d68dcbec2cbe6fcdbe4d70619769d5e8786ea66a
Giffen-good <chris6rock@gmail.com>
2022-03-18 17:55:08 -0400

    project 89

--- index.js
+++ index.js
@@ -3,36 +3,51 @@
 const path = require('path');
 const fs = require('fs');
 //joining path of directory
-const sourceDir = path.join(__dirname, 'assets');
-const destDir = path.join(__dirname, 'modified-assets');
+const destDir = path.join(__dirname, 'metadata');
+
 if (!fs.existsSync(destDir)){
     fs.mkdirSync(destDir);
 }
-//passsing directoryPath and callback function
-fs.readdir(sourceDir, function (err, files) {
-    //handling error
-    if (err) {
-        return console.log('Unable to scan directory: ' + err);
-    }
-    //listing all files using forEach
-    files.forEach(function (file) {
-        // Do whatever you want to do with the file
-        console.log(file);
-        const ext = file.split('.').pop();
 
-        if (ext === 'json') {
-            const data = require(`${sourceDir}/${file}`);
-            // where the magic happens
-            renameKey( data, 'image', 'animation_url' );
-
-
-            storeData(data,`${destDir}/${file}`);
-            console.log(data)
-        } else {
-            if (!fs.existsSync(`${destDir}/${file}`)) copyFile(`${sourceDir}/${file}`, `${destDir}/${file}`)
+const raw = `{
+    "name": "Founders Pass #001",
+    "symbol": "FP-P89",
+    "description": "Collection of 333 Founders Passes, Which Grant You Special Perks Within Project89",
+    "seller_fee_basis_points": 500,
+    "image": "0.png",
+    "animation_url": "0.mp4",
+    "external_url": "https://project89.io",
+    "attributes": [
+      {
+        "trait_type": "Founder Tier",
+        "value": "Diamond"
+      }
+    ],
+    "collection": {
+       "name": "Founders Pass",
+       "family": "Project89"
+    },
+    "properties": {
+      "files": [
+        {
+          "uri": "0.png",
+          "type": "image/png"
+        },
+        {
+          "uri": "0.mp4",
+          "type": "video/mp4"
         }
-    });
-});
+      ],
+      "category": "video",
+      "creators": [
+        {
+          "address": "6QH3FzZFgdgQmK7Fi36TcTdAh5EH6NN2NwNjjbNpcFiQ",
+          "share": 100
+        }
+      ]
+    }
+  }`
+
 const renameKey = ( obj, oldKey, newKey ) => {
     obj[newKey] = obj[oldKey];
     delete obj[oldKey];
@@ -45,8 +60,71 @@ const copyFile = (source, dest) => {
 }
 const storeData = (data, path) => {
     try {
-        fs.writeFileSync(path, JSON.stringify(data))
+        fs.writeFileSync(path, JSON.stringify(data, null, 4))
     } catch (err) {
         console.error(err)
     }
 }
+function getlength(number) {
+    return number.toString().length;
+}
+function writeProject89Meta() {
+    for (let i = 0;i <= 332; i++) {
+        let tier, zeroes;
+        switch (i)
+        {
+            case i < 16:
+                tier = 'Diamond';
+                break;
+    
+            case i < 65:
+                tier = 'Gold';
+                break;
+            case i < 106:
+                tier = 'Silver';
+                break;
+            
+            default:
+                tier = 'Bronze'
+                break;
+        }
+        // check if number needs 0's in front of it
+        const numberOfDigits = getlength(i+1);
+        switch (numberOfDigits)
+        {
+            case 1:
+                zeroes = '00';
+                break
+            case 2:
+                zeroes = '0';
+                break
+            default:
+                zeroes = '';
+                break
+        }
+        
+        const json = JSON.parse(raw);
+    
+        //Fields that we will modify
+        const name = `Founders Pass #${zeroes}${i+1}`;
+        const image = `${i}.png`;
+        const animation = `${i}.mp4`;
+    
+        json['name'] = name;
+        json['image'] = image;
+        json['animation_url'] = animation;
+        json['attributes'][0].value = tier;
+        json['properties']['files'][0]['uri'] = image;
+        json['properties']['files'][1]['uri'] = animation;
+    
+    
+        storeData(json,`${destDir}/${i}.json`);
+        console.log(json)
+    
+    }
+}
+
+
+writeProject89Meta();
+
+
